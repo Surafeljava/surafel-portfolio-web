534b6cb77050e001ea6546d8fd801cab5a6c3892
Surafeljava <surafelk27@gmail.com>
2022-09-01 00:39:59 +0300

    contact

--- src/components/HomePage/Contact.js
+++ src/components/HomePage/Contact.js
@@ -7,16 +7,29 @@ function Contact() {
                 <FaChevronRight className="text-transparent group-hover:text-red-500 font-normal text-sm group-hover:text-xl md:text-2xl inline -ml-8 group-hover:ml-0 duration-500"/> 
                 Let's Chat
             </div>
-            <div className="flex flex-col gap-2">
-                <div className="text-xl font-semibold text-slate-600">
-                    +251 942 85 97 23
+            <div className="flex">
+                <div className="flex flex-col gap-2">
+                    <div className="text-xl font-semibold text-slate-600">
+                        +251 942 85 97 23
+                    </div>
+                    <div className="text-md font-normal text-slate-500">
+                        Addis Ababa, Ethiopia
+                    </div>
+                    <div className="flex my-8">
+                        <div className="flex group items-center">
+                            <FaChevronRight className='w-2 h-2 group-hover:w-4 group-hover:h-4 invisible -ml-2 group-hover:ml-0 group-hover:visible duration-200'/>
+                            <a href="mailto:surafel.mulaw@gmail.com" 
+                            className="px-4 py-2 rounded-lg font-roboto text-darker-1 text-md bg-yellow-1 ml-0 group-hover:ml-2 group-hover:bg-darker-1 group-hover:text-light-1 duration-200">
+                                Send an Email
+                            </a>
+                        </div>
+                    </div>
                 </div>
-                <div className="text-md font-normal text-slate-500">
-                    Addis Ababa, Ethiopia
-                </div>
-                <div className="flex my-8">
+
+                <div className="flex flex-col gap-2">
+                    <input type="email" className="rounded-lg" placeholder="Email" />
+                    <input type="text" className="rounded-lg" placeholder="Message..." />
                     <div className="flex group items-center">
-                        <FaChevronRight className='w-2 h-2 group-hover:w-4 group-hover:h-4 invisible -ml-2 group-hover:ml-0 group-hover:visible duration-200'/>
                         <a href="mailto:surafel.mulaw@gmail.com" 
                         className="px-4 py-2 rounded-lg font-roboto text-darker-1 text-md bg-yellow-1 ml-0 group-hover:ml-2 group-hover:bg-darker-1 group-hover:text-light-1 duration-200">
                             Send an Email
